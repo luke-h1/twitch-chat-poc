4270cfeb02cdcc66608ab43a4950d7c62d0a1895
luke-h1 <luke.howsam@yahoo.com>
2024-12-28 15:29:51 +0000

    --wip-- [skip ci]

--- src/store/slices/emotes/types.ts
+++ src/store/slices/emotes/types.ts
@@ -6,3 +6,15 @@ export interface Emoji {
   codePoints: string;
   variants: string[];
 }
+
+export interface AllEmotes {
+  twitch?: Emotes<TwitchEmote>;
+  twitchTemplate?: string;
+  bttvGlobal?: Emotes<BttvEmote>;
+  bttvChannel?: Emotes<BttvEmote>;
+  ffzGlobal?: Emotes<FfzEmote>;
+  ffzChannel?: Emotes<FfzEmote>;
+  stvGlobal?: Emotes<StvEmote>;
+  stvChannel?: Emotes<StvEmote>;
+  emoji?: Emotes<Emoji>;
+}
